cd61ce12d484c11b9b403d8821574c43579769a4
Robert Udrea <udrearobert999@gmail.com>
2026-03-01 01:08:01 +0200

    Enhanced fugitive config and remaped commands

--- lua/robert/plugins/git.lua
+++ lua/robert/plugins/git.lua
@@ -1,15 +1,33 @@
 -- lua/robert/plugins/git.lua
 
 return {
-  {
-    'tpope/vim-fugitive',
-    cmd = {
-      'Git',
-      'Gstatus',
-      'Gcommit',
-      'Gpush',
-      'Gpull',
-      'Gdiffsplit',
-    },
-  },
+  'tpope/vim-fugitive',
+  config = function()
+    vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+
+    local robert_fugitive_group = vim.api.nvim_create_augroup('robert-fugitive-group', {})
+
+    local autocmd = vim.api.nvim_create_autocmd
+    autocmd('BufWinEnter', {
+      group = robert_fugitive_group,
+      pattern = '*',
+      callback = function()
+        if vim.bo.ft ~= 'fugitive' then return end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = { buffer = bufnr, remap = false }
+        vim.keymap.set('n', '<leader>p', function() vim.cmd.Git 'push' end, opts)
+
+        -- rebase always
+        vim.keymap.set('n', '<leader>P', function() vim.cmd.Git { 'pull', '--rebase' } end, opts)
+
+        -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+        -- needed if i did not set the branch up correctly
+        vim.keymap.set('n', '<leader>t', ':Git push -u origin ', opts)
+      end,
+    })
+
+    vim.keymap.set('n', 'gu', '<cmd>diffget //2<CR>')
+    vim.keymap.set('n', 'gh', '<cmd>diffget //3<CR>')
+  end,
 }
